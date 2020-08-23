9bba54f717b760745ada361cd2f1e208e6a3cee3
Raphael3c <phael.ufc@gmail.com>
2020-08-23 14:44:37 -0300

    2° commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,11 @@
-<script>
-    function backCollor1() {
-        
-    }
+ function backCollor1() {
+    document.bgColor = '#c2ac31'
+}
 
-    function backCollor2() {
-        
-    }
+function backCollor2(){
+    document.bgColor = '#000000'
+}
 
-    function backCollor3() {
-        
-    }
-</script>
+function backCollor3(){
+    document.bgColor = '#ec2628'
+}
