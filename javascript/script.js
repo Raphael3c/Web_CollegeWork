e79f23ed3a6005325bf300b486b65a1fbb5c4246
Raphael3c <phael.ufc@gmail.com>
2020-08-24 18:10:40 -0300

    7° commit

--- javascript/script.js
+++ javascript/script.js
@@ -4,16 +4,24 @@ function defaultColor(){
 
 function backColor1(){
     document.bgColor = '#000000'
+    document.getElementById("buttonCredit").style.backgroundColor = "#9248ff"
 }
 
 function backColor2(){
     document.bgColor = '#9248ff'
+    document.getElementById("buttonCredit").style.backgroundColor = "#000000"
 }
 
 function backColor3(){
     document.bgColor = '#ffffff'
+    document.getElementById("buttonCredit").style.backgroundColor = "#000000"
 }
 
 function devOps() {
     varWindow = window.open('html/devops.html', 'popup')
+}
+
+
+function registerMensage(){
+
 }
