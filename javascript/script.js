3b63b8c4fa381da1486fb06ae775feccfd7df166
Raphael3c <phael.ufc@gmail.com>
2020-08-25 12:43:18 -0300

    i have add the "chat" whit DOM, but we have more things to do: we need to reveal the style!

--- javascript/script.js
+++ javascript/script.js
@@ -21,7 +21,7 @@ function devOps() {
     varWindow = window.open('html/devops.html', 'popup')
 }
 
-function registerMensage(){
+/*function registerMensage(){
     var data = new Array()
 
     if (localStorage.hasOwnProperty("data")) {
@@ -37,4 +37,52 @@ function registerMensage(){
     })
 
     localStorage.setItem("data", JSON.stringify(data))
+}
+
+function ccc(){
+    var data = JSON.parse(localStorage.getItem("data"))
+    var mensage = document.getElementById("mensages")
+    mensage.innerHTML = data[0].name
+}*/
+
+function registerMensage(){ 
+
+   var nome = document.getElementById("name")
+   var mensage = document.getElementById("mensage")
+   var board = document.getElementById("mensageBoard")
+   var div = document.createElement("div")
+   var p = document.createElement("p")
+   var p1 = document.createElement("p")
+   var button = document.createElement("button")
+
+   button.innerHTML = "Excluir"
+
+   var eMensage = `${nome.value}: ${mensage.value}`
+   
+   board.appendChild(div)
+   div.appendChild(p)
+   div.appendChild(button)
+   p.innerHTML = eMensage
+   button.onclick = function(){
+       div.style.display = "none"
+   }
+
+   div.scrollTop = div.scrollHeight
+
+   div.style.position = "relative"
+   //div.style.bottom = "150px"
+   div.style.left = "10px"
+   div.style.backgroundColor = "#696969"
+   div.style.borderRadius = "10px"
+   div.style.marginRight = "20px"
+
+   p.style.position = "relative"
+   p.style.top = "25px"
+   p.style.left = "10px"
+   p.style.fontSize = "20px"
+
+   button.style.position = "relative"
+   button.style.bottom = "23px"
+   button.style.left = "450px"
+   
 }
